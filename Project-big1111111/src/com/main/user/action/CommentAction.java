e0af46a537e9b8993792423438d2aff357c7b51a
wangfengduo <958963326@qq.com>
2018-05-29 16:34:14 +0800

    add

--- Project-big1111111/src/com/main/user/action/CommentAction.java
+++ Project-big1111111/src/com/main/user/action/CommentAction.java
@@ -1,5 +1,18 @@
 package com.main.user.action;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.main.user.service.CommentService;
+
+@Controller
+@RequestMapping("/user")
 public class CommentAction {
+	@Autowired
+	private CommentService commentService;
+	//获取所有评论信息，分页显示
+	
+	
 
 }
